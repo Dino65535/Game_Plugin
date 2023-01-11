f26113fa8df9769b854ca03fa50f2f246de91b62
Dino65535 <sad20001008@gmail.com>
2023-01-11 16:17:20 +0800

    Update Plugin file

---
+++ Plugin/Plugin/TestPlugin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+
+namespace Plugin
+{
+    [BepInPlugin("Dino.first.plugin", "Test Plugin", "8.0")] //plugin描述  ID.名子.版本
+    public class TestPlugin : BaseUnityPlugin //繼承
+    {
+        void Awake()
+        {
+            Logger.LogInfo("Hello World"); //log print
+        }
+    }
+}
