e5f5055d9ff19a9ab592412fe072906c80224f84
Dino65535 <sad20001008@gmail.com>
2023-01-13 04:30:05 +0800

    Update plugin

--- Plugin/Hello World Plugin/Hello World Plugin/Plugin.cs
+++ Plugin/Hello World Plugin/Hello World Plugin/Plugin.cs
@@ -4,15 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BepInEx;
+using BepInEx.Configuration;
 
 namespace Plugin
 {
     [BepInPlugin("Dino.first.plugin", "Test Plugin", "1.0")] //plugin描述  ID.名子.版本
     public class Plugin : BaseUnityPlugin //繼承
     {
+        ConfigEntry<int> intConfig;
+        ConfigEntry<string> stringConfig;
         void Awake()
         {
             Logger.LogInfo("Hello World!"); //log print
+
+            //show in option, key name in file, default value, description
+            intConfig = Config.Bind<int>("config", "TestInt", 20, "測試用整數");
+            stringConfig = Config.Bind<string>("config", "TestString", "Hello World!!", "測試用字串");
+
+            Logger.LogInfo("int " + intConfig.Value);
+            Logger.LogInfo("string " + stringConfig.Value);
+        }
+
+        
+
+        void Start()
+        {
+            Logger.LogInfo("plugin is success");
+        }
+        void OnDestory()
+        {
+            Logger.LogInfo("plugin is success");
         }
     }
 }
