70b2fe365ea5d577f04250f81f8a23ff1da3b4af
Dino65535 <sad20001008@gmail.com>
2023-01-14 04:13:48 +0800

    plugin update

--- Plugin/Hello World Plugin/Hello World Plugin/Plugin.cs
+++ Plugin/Hello World Plugin/Hello World Plugin/Plugin.cs
@@ -5,35 +5,40 @@ using System.Text;
 using System.Threading.Tasks;
 using BepInEx;
 using BepInEx.Configuration;
+using Kitchen;
+using Kitchen.Modules;
+using UnityEngine;
+using System.Reflection;
+using KitchenLib;
 
 namespace Plugin
 {
-    [BepInPlugin("Dino.first.plugin", "Test Plugin", "1.0")] //plugin描述  ID.名子.版本
-    public class Plugin : BaseUnityPlugin //繼承
+    //[BepInPlugin("Dino.first.plugin", "Test Plugin", "3.0")] //plugin描述  ID.名子.版本
+    [BepInProcess("PlateUp.exe")]
+    [BepInPlugin("Dino.test.plugin", "Test Plugin", "1.0.0")]
+    public class Plugin : BaseMod //繼承
     {
+        public Plugin() : base("Dino.test.plugin", "Test Plugin", "Dino", "1.0.0", ">=1.1.1", Assembly.GetExecutingAssembly()) { }
+
+        //protected override void OnFrameUpdate() { }
+
+        //protected override void OnInitialise() { }
+
+        /*
         ConfigEntry<int> intConfig;
         ConfigEntry<string> stringConfig;
         void Awake()
         {
             Logger.LogInfo("Hello World!"); //log print
 
-            //show in option, key name in file, default value, description
+            show in option, key name in file, default value, description
             intConfig = Config.Bind<int>("config", "TestInt", 20, "測試用整數");
             stringConfig = Config.Bind<string>("config", "TestString", "Hello World!!", "測試用字串");
 
             Logger.LogInfo("int " + intConfig.Value);
             Logger.LogInfo("string " + stringConfig.Value);
-        }
+        }*/
 
-        
 
-        void Start()
-        {
-            Logger.LogInfo("plugin is success");
-        }
-        void OnDestory()
-        {
-            Logger.LogInfo("plugin is success");
-        }
     }
 }
